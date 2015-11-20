9349c509db83061b39f3951ca5fc6c446c46950d
Gina Gibsen <ggibsen@kpmg.com>
2015-11-20 11:44:00 -0800

    for further merge tests

---
+++ src/js/playground.js
@@ -0,0 +1,17 @@
+(function($) {
+	
+	// Fade in the page...
+	$(document).ready(function() {
+		var $body = $('body');
+		$body.hide();
+		$(window).on('load', function() {
+			$body.fadeIn(1000);
+		});
+	});
+	
+	
+	function merge1() {
+		
+	}
+	
+}(jQuery));
